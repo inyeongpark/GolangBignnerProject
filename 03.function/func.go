ec3f418c7ef52255491d56343e2b5bd507822d0a
Park In Yeong <hsd3030@gmail.com>
2022-06-19 23:59:13 +0900

    multi return funct예제

--- 03.function/func.go
+++ 03.function/func.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 //  int a int b는 함수가 외부로부터 입력받는 값 매개변수
-//
+
 func Add(a int, b int) int {
 	return a + b
 }
@@ -13,3 +13,5 @@ func main() {
 	c := Add(3, 6)
 	fmt.Println(c)
 }
+
+//  핵심으로는 인수는 매개변수로 복사되며 함수내에서 선언된 변수는 함수 종료시에 접근안된다.
